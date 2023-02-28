12ea777c6722f872ffa93bb0193c6f16916cc6f1
Azizul Hakim Arif <azizulhakimarif7@gmail.com>
2023-03-01 00:49:57 +0600

    EF CodeFirst

---
+++ EFCodeFirst/EFCodeFirst/EF/PMSDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using EFCodeFirst.EF.Models;
+
+namespace EFCodeFirst.EF
+{
+    public class PMSDb :DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
